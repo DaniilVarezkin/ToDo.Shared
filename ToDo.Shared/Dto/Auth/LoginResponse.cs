26bb009e4b5b423086f6b26c96aa12ad9dbd156c
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-06-13 14:29:05 +0300

    Добавлены страницы списка задач и страница добаления задачи

--- ToDo.Shared/Dto/Auth/LoginResponse.cs
+++ ToDo.Shared/Dto/Auth/LoginResponse.cs
@@ -1,5 +1,8 @@
 ﻿namespace ToDo.Shared.Dto.Auth
 {
+    /// <summary>
+    /// DTO возвращаемое при входе в аккаунт
+    /// </summary>
     public class LoginResponse
     {
         public string Token { get; set; } = default!;
